8d940251681386e339b2072696e638a8300d5a50
David Puerta Martos <dpuerta@usal.es>
2023-02-14 20:19:24 +0100

    basic map showing all species observed in poland (only first 5000)

--- app.R
+++ app.R
@@ -1,51 +1,32 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(leaflet)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+df <- read.csv(file="~/SpeciesMap/data/filteredPoland.csv",
+               nrows=5000,
+               skip=0,
+               header=TRUE,
+               sep=",")
+# df <- arrow::read_parquet("~/SpeciesMap/data/filteredPoland.parquet") 
+print(head(df))
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+ui <- bootstrapPage(
+  #front end
+  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+  leafletOutput("map", 
+                width = "100%", 
+                height = "100%"),
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+server <- function(input, output, session) {
+  #back end
+  output$map <- renderLeaflet({
+    leaflet(data=df, options = leafletOptions(preferCanvas = TRUE)) %>%
+      addTiles() %>%
+      addMarkers(~longitudeDecimal, 
+                 ~latitudeDecimal)
+  })
+  
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
