c3b5bd8b7a89252126220c856499f1dc9ce3c59f
David Puerta Martos <dpuerta@usal.es>
2023-02-16 00:11:35 +0100

    renamed variable to now interfere with base functions

--- server/filter_data.R
+++ server/filter_data.R
@@ -1,21 +1,21 @@
 filter_data <- function(df, years, sci_name, vern_name) {
   #filter dataframe with selected years
-  base <- df %>%
+  data_base <- df %>%
     dplyr::filter(year >= years[1],
                   year <= years[2])
   
   #if there's a filter active for scientific name, filter dataframe
   if(!is.null(sci_name) & sci_name != ""){
-    base <- base %>%
+    data_base <- data_base %>%
       filter(str_detect(species_list, sci_name))
   }
   
   #if there's a filter active for vernacular name, filter dataframe
   if(!is.null(vern_name) & vern_name != ""){
-    base <- base %>%
+    data_base <- data_base %>%
       filter(str_detect(vernacular_name, vern_name))
   }
   
   #return dataframe with selected filters applied
-  return(base)
+  return(data_base)
 }
